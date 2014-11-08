300252cbef92c427d201fbdf068f04bd24362e3c
cyberabis <babishek@gmail.com>
2014-11-09 01:39:36 +0530

    Search API without HBM completed

--- rangde-api/src/com/rangde/api/SearchResource.java
+++ rangde-api/src/com/rangde/api/SearchResource.java
@@ -28,8 +28,22 @@ public class SearchResource {
 	@Produces({ MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML })
 	public List<Patient> search() {
 		List<Patient> patients = new ArrayList<Patient>();
-		Patient patient1 = new Patient("Abishek");
-		Patient patient2 = new Patient("Kousik");
+		Patient patient1 = new Patient();
+		patient1.setImageSrc("http://www.rangde.org/images/landingPage/madness_pro_img7.jpg");
+		patient1.setAmountRaised(10000);
+		patient1.setAmountNeeded(20000);
+		patient1.setAmountRemaining(1000);
+		patient1.setPercentageRaised(25);
+		patient1.setTimeLeft("3 days");
+		patient1.setPatientName("Kidney Transplant for Mohan");
+		Patient patient2 = new Patient();
+		patient1.setImageSrc("http://www.rangde.org/images/landingPage/madness_pro_img7.jpg");
+		patient1.setAmountRaised(10000);
+		patient1.setAmountNeeded(20000);
+		patient1.setAmountRemaining(1000);
+		patient1.setPercentageRaised(25);
+		patient1.setTimeLeft("3 days");
+		patient1.setPatientName("Kidney Transplant for Kumar");
 		patients.add(patient1);
 		patients.add(patient2);
 		return patients;
@@ -48,6 +62,25 @@ public class SearchResource {
 		System.out.println("Search name received: " + search.getTimeLeft());
 		System.out.println("Search location received: " + search.getVolunteer());
 		System.out.println("Search name received: " + search.getCategories());
+		
+		Patient patient1 = new Patient();
+		patient1.setImageSrc("http://www.rangde.org/images/landingPage/madness_pro_img7.jpg");
+		patient1.setAmountRaised(10000);
+		patient1.setAmountNeeded(20000);
+		patient1.setAmountRemaining(1000);
+		patient1.setPercentageRaised(25);
+		patient1.setTimeLeft("3 days");
+		patient1.setPatientName("Kidney Transplant for Mohan");
+		Patient patient2 = new Patient();
+		patient2.setImageSrc("http://www.rangde.org/images/landingPage/madness_pro_img7.jpg");
+		patient2.setAmountRaised(10000);
+		patient2.setAmountNeeded(20000);
+		patient2.setAmountRemaining(1000);
+		patient2.setPercentageRaised(25);
+		patient2.setTimeLeft("3 days");
+		patient2.setPatientName("Kidney Transplant for Kumar");
+		patients.add(patient1);
+		patients.add(patient2);
 		
 		return patients;
 	}
--- rangde-api/src/com/rangde/api/model/Patient.java
+++ rangde-api/src/com/rangde/api/model/Patient.java
@@ -5,21 +5,68 @@ import javax.xml.bind.annotation.XmlRootElement;
 @XmlRootElement
 public class Patient {
 	
-	private String name;
+	private String imageSrc;
+	private float amountRaised;
+	private float amountNeeded;
+	private float amountRemaining;
+	private float percentageRaised;
+	private String timeLeft;
+	private String patientName;
 	
 	public Patient(){	
 	}
 	
-	public Patient(String name) {
-		this.name = name;
+	public Patient(String imageSrc, float amountRaised, float amountNeeded, float amountRemaining, float percentageRaised, String timeLeft, String patientName){
+		this.imageSrc = imageSrc;
+		this.amountRaised = amountRaised;
+		this.amountNeeded = amountNeeded;
+		this.amountRemaining = amountRemaining;
+		this.percentageRaised = percentageRaised;
+		this.timeLeft = timeLeft;
+		this.patientName = patientName;
 	}
-
-	public String getName() {
-		return name;
+	
+	public String getImageSrc() {
+		return imageSrc;
 	}
-
-	public void setName(String name) {
-		this.name = name;
+	public void setImageSrc(String imageSrc) {
+		this.imageSrc = imageSrc;
 	}
-
+	public float getAmountRaised() {
+		return amountRaised;
+	}
+	public void setAmountRaised(float amountRaised) {
+		this.amountRaised = amountRaised;
+	}
+	public float getAmountNeeded() {
+		return amountNeeded;
+	}
+	public void setAmountNeeded(float amountNeeded) {
+		this.amountNeeded = amountNeeded;
+	}
+	public float getAmountRemaining() {
+		return amountRemaining;
+	}
+	public void setAmountRemaining(float amountRemaining) {
+		this.amountRemaining = amountRemaining;
+	}
+	public float getPercentageRaised() {
+		return percentageRaised;
+	}
+	public void setPercentageRaised(float percentageRaised) {
+		this.percentageRaised = percentageRaised;
+	}
+	public String getTimeLeft() {
+		return timeLeft;
+	}
+	public void setTimeLeft(String timeLeft) {
+		this.timeLeft = timeLeft;
+	}
+	public String getPatientName() {
+		return patientName;
+	}
+	public void setPatientName(String patientName) {
+		this.patientName = patientName;
+	}
+	
 }
