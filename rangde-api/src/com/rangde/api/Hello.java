f84d4351d6aac63f9c486a3d77a4b9c6362e8998
cyberabis <babishek@gmail.com>
2014-11-08 18:16:52 +0530

    Tested serving HTML

--- rangde-api/src/com/rangde/api/Hello.java
+++ rangde-api/src/com/rangde/api/Hello.java
@@ -14,19 +14,4 @@ public class Hello {
     return "Hello Jersey";
   }
 
-  // This method is called if XML is request
-  @GET
-  @Produces(MediaType.TEXT_XML)
-  public String sayXMLHello() {
-    return "<?xml version=\"1.0\"?>" + "<hello> Hello Jersey" + "</hello>";
-  }
-
-  // This method is called if HTML is request
-  @GET
-  @Produces(MediaType.TEXT_HTML)
-  public String sayHtmlHello() {
-    return "<html> " + "<title>" + "Hello Jersey" + "</title>"
-        + "<body><h1>" + "Hello Jersey" + "</body></h1>" + "</html> ";
-  }
-
 } 
