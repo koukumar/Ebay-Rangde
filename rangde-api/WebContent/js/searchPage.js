d7b4086946125e5f1691104cff21b5c3dc2ca853
Suresh Pattu <sureshpattu2002@gmail.com>
2014-11-09 11:29:00 +0530

    Search page UI design is done

--- rangde-api/WebContent/js/searchPage.js
+++ rangde-api/WebContent/js/searchPage.js
@@ -1,47 +1,66 @@
 var searchPage = {
     initialized: false,
     init: function() {
-        searchPage.initialized = true;
-        var filters = $('[name="categories"],[name="volunteer"],[name="location"],[name="language"],[name="timeLeft"],[name="percentFunded"],[name="fundingStatus"]');
-        filters.click(function() {
+    	if(searchPage.initialized != true){
+    		searchPage.initialized = true;
+            var filters = $('[name="categories"],[name="volunteer"],[name="location"],[name="language"],[name="timeLeft"],[name="percentFunded"],[name="fundingStatus"]');
+            filters.click(function() {
+                searchPage.filterCall();
+            });
             searchPage.filterCall();
-        });
-        searchPage.filterCall();
+            $('.modernCheckBoxWrapper').each(function () {
+                var element = $(this).attr('data-bind');
+                if ($(element).is(":checked")) {
+                    $(this).addClass('active');
+                } else {
+                    $(this).removeClass('active');
+                }
+            });
+            $('.modernRadioButtonWrapper').each(function () {
+                var element = $(this).attr('data-bind');
+                if ($(element).is(":checked")) {
+                    $(this).addClass('active');
+                } else {
+                    $(this).removeClass('active');
+                }
+            });
+            $('.modernCheckBoxWrapper').click(function () {
+                $(this).toggleClass('active');
+                var element = $(this).attr('data-bind');
+                $(element).trigger('click');
+            });
+            $('.modernRadioButtonWrapper').click(function () {
+                var elementGroup = $(this).attr('data-bind-group'),
+                    element = $(this).attr('data-bind');
+                $('[name=' + elementGroup + ']').attr("checked", false);
+                $('[data-bind-group=' + elementGroup + ']').removeClass('active');
+                if (!$(element).is(':checked')) {
+                    $(this).toggleClass('active');
+                    $(element).attr("checked", true);
+                }
+            });
+    	}        
     },
     filterCall: function() {
         var onFilterSuccess = function(data) {
-            /*var searchResults = [{
-                'imageSrc': 'http://www.rangde.org/images/landingPage/madness_pro_img7.jpg',
-                'amountRaised': '1000',
-                'amountNeeded': '10,000',
-                'amountRemaining': '9,000',
-                'percentageRaised': '10',
-                'location': 'Bangalore',
-                'timeLeft': '2 Days',
-                'patientName': 'Suresh'
-            }, {
-                'imageSrc': 'http://www.rangde.org/images/landingPage/madness_pro_img7.jpg',
-                'amountRaised': '1000',
-                'amountNeeded': '10,000',
-                'amountRemaining': '9,000',
-                'percentageRaised': '10',
-                'location': 'Bangalore',
-                'timeLeft': '2 Days',
-                'patientName': 'Suresh'
-            }];*/
+        	var listCount = 0;
             $.get('js/templates/searchPageTemplate.mst', function(patientDescriptionSecTemplate) {
                 $.each(data, function(i, field) {
-                    var patientDescriptionSecTemplateRendered = Mustache.render(patientDescriptionSecTemplate, {
-                        imageSrc: field.imageSrc,
-                        amountRaised: field.amountRaised,
-                        amountNeeded: field.amountNeeded,
-                        amountRemaining: field.amountRemaining,
-                        percentageRaised: field.percentageRaised,
-                        locations: field.locations,
-                        timeLeft: field.timeLeft,
-                        patientName: field.patientName
-                    });
-                    $('.searchResultsDisplaySec').append(patientDescriptionSecTemplateRendered);
+                	if(listCount < 9){
+                		listCount++;
+                		var patientDescriptionSecTemplateRendered = Mustache.render(patientDescriptionSecTemplate, {
+                            imageSrc: field.imageSrc,
+                            amountRaised: field.amountRaised,
+                            amountNeeded: field.amountNeeded,
+                            amountRemaining: field.amountRemaining,
+                            percentageRaised: field.percentageRaised,
+                            locations: field.locations,
+                            timeLeft: field.timeLeft,
+                            patientName: field.patientName
+                        });                    
+                        $('.searchResultsDisplaySec').append(patientDescriptionSecTemplateRendered);
+                	}
+                    
                 });
             });
 
