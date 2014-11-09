cf7ca9b4b5fe98b4242344a7a9e9df1fe6c23aad
cyberabis <babishek@gmail.com>
2014-11-09 07:08:37 +0530

    Added more demo data

--- rangde-api/src/com/rangde/api/SearchResource.java
+++ rangde-api/src/com/rangde/api/SearchResource.java
@@ -54,34 +54,18 @@ public class SearchResource {
 	@Produces({ MediaType.APPLICATION_JSON })
 	public List<Patient> findPatients(Search search) {
 		List<Patient> patients = new ArrayList<Patient>();
-		
-		System.out.println("Search name received: " + search.getFundingStatus());
-		System.out.println("Search location received: " + search.getLanguage());
-		System.out.println("Search name received: " + search.getLocation());
-		System.out.println("Search location received: " + search.getPercentFunded());
-		System.out.println("Search name received: " + search.getTimeLeft());
-		System.out.println("Search location received: " + search.getVolunteer());
-		System.out.println("Search name received: " + search.getCategories());
-		
-		Patient patient1 = new Patient();
-		patient1.setImageSrc("http://www.rangde.org/images/landingPage/madness_pro_img7.jpg");
-		patient1.setAmountRaised(10000);
-		patient1.setAmountNeeded(20000);
-		patient1.setAmountRemaining(1000);
-		patient1.setPercentageRaised(25);
-		patient1.setTimeLeft("3 days");
-		patient1.setPatientName("Kidney Transplant for Mohan");
-		Patient patient2 = new Patient();
-		patient2.setImageSrc("http://www.rangde.org/images/landingPage/madness_pro_img7.jpg");
-		patient2.setAmountRaised(10000);
-		patient2.setAmountNeeded(20000);
-		patient2.setAmountRemaining(1000);
-		patient2.setPercentageRaised(25);
-		patient2.setTimeLeft("3 days");
-		patient2.setPatientName("Kidney Transplant for Kumar");
-		patients.add(patient1);
-		patients.add(patient2);
-		
+		Patient patient1 = null;
+		for(int i = 0; i< 15; i++){
+			patient1 = new Patient();
+			patient1.setImageSrc("http://www.rangde.org/images/landingPage/madness_pro_img7.jpg");
+			patient1.setAmountRaised(10000);
+			patient1.setAmountNeeded(20000);
+			patient1.setAmountRemaining(1000);
+			patient1.setPercentageRaised(25);
+			patient1.setTimeLeft("3 days");
+			patient1.setPatientName("Kidney Transplant for Mohan");
+			patients.add(patient1);
+		}
 		return patients;
 	}
 
