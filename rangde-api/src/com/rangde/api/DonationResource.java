59e3c677a945a5ab2283db99ec2524b90a1f641c
cyberabis <babishek@gmail.com>
2014-11-09 12:27:32 +0530

    Donotaion API

--- rangde-api/src/com/rangde/api/DonationResource.java
+++ rangde-api/src/com/rangde/api/DonationResource.java
@@ -3,9 +3,7 @@ package com.rangde.api;
 import java.util.ArrayList;
 import java.util.List;
 
-import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
-import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
@@ -14,11 +12,11 @@ import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Request;
 import javax.ws.rs.core.UriInfo;
 
-import com.rangde.api.model.Patient;
+import com.rangde.api.model.Donation;
 
-@Path("/patient")
-public class PatientResource {
-
+@Path("/donation/patient")
+public class DonationResource {
+	
 	@Context
 	UriInfo uriInfo;
 	@Context
@@ -27,11 +25,13 @@ public class PatientResource {
 	@GET
 	@Path("{id}")
 	@Produces({ MediaType.APPLICATION_JSON })
-	public Patient getPatient(@PathParam("id") Integer id) {
-		System.out.println("Fetching patient:" + id);
-		Patient patient = SearchResource.patients.get(id);
-		return patient;
+	public List<Donation> getDonation(@PathParam("id") Integer id) {
+		List<Donation> donations = new ArrayList<Donation>();
+		System.out.println("Fetching donation for patient:" + id);
+		for(int i = 0; i<3; i++){
+			donations.add(new Donation(id, i + 1000, "Suresh", "Get well soon!", "3 days ago"));
+		}
+		return donations;
 	}
 
 }
-
